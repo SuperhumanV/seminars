266ee1d7923c07546ed122d0e213cd91b62c7845
Vad <poriata@yandex.ru>
2022-09-12 21:13:45 +0300

    task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? inputLine1 = Console.ReadLine();
+string? inputLine2 = Console.ReadLine();
+if(inputLine1 != null&&inputLine2 != null)
+{
+    int inputNumber1 = int.Parse(inputLine1);
+    int inputNumber2 = int.Parse(inputLine2);
+
+    bool outResult = (inputNumber1*inputNumber1==inputNumber2*inputNumber2);
+     
+Console.WriteLine(outResult);
+}
+
